fe03e40f8f659fbee01ba0386daff0c4f4f91e82
Eugene Butkov <eugenefromrus@gmail.com>
2023-09-03 13:04:54 +1000

    feat: fix merge

--- src/shared/http/index.ts
+++ src/shared/http/index.ts
@@ -1,11 +1,6 @@
 import axios from "axios"
 
 export const http = axios.create({
-<<<<<<< HEAD
-  withCredentials: true,
-  baseURL: "http://213.189.201.213:3000",
-=======
   // withCredentials: true,
   baseURL: "http://213.189.201.213:7000",
->>>>>>> 4128320b67b62135957d6d534a359d24f1fcf436
 })
