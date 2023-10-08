6fe56fadf567c5a0adcf81596863fff30f4eb055
Eugene Butkov <eugenefromrus@gmail.com>
2023-10-08 23:19:55 +1000

    fix: fix http settings

--- src/shared/http/index.ts
+++ src/shared/http/index.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 import { AVAILABLE_LS_KEYS } from "../constants"
 
 const http = axios.create({
-  baseURL: process.env.NODE_ENV === "production" ? "http://213.189.201.213:8000" : "https://monoframe.ru",
+  baseURL: process.env.NODE_ENV === "development" ? "http://213.189.201.213:8000" : "https://monoframe.ru",
   withCredentials: process.env.NODE_ENV === "development" ? false : true,
 })
 
