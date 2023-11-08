75c865046e167e8c9fc72379bf1e4440b24d4c4d
Eugene Butkov <eugenefromrus@gmail.com>
2023-11-08 21:59:52 +1000

    fix: fix parseHttp

--- src/shared/helpers/praseUrlToFile.ts
+++ src/shared/helpers/praseUrlToFile.ts
@@ -1,7 +1,7 @@
 export async function praseUrlToFile(url: string, fileName: string) {
   let parseUrl = url
   if (process.env.NODE_ENV === "production") parseUrl.replace("http:", "https:")
-  const response = await fetch(url, {
+  const response = await fetch(parseUrl, {
     mode: "no-cors",
   })
   const blob = await response.blob()
